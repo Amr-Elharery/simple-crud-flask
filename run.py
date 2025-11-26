a79f19c46e20e52f6554335358ca78f74e73e590
Amr Tamer Elharery <amrelharery@gmail.com>
2025-11-26 16:51:44 +0200

    Add PORT from env

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from src import create_app
+from src.config.config import Config
 
 app = create_app()
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, port=Config.PORT)
--- src/config/config.py
+++ src/config/config.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 class Config:
   SECRET_KEY = os.getenv("SECRET_KEY","dev")
+  PORT = int(os.getenv("PORT", 5000))
   
   DB_USER = os.getenv("DB_USER", "root")
   DB_PASSWORD = os.getenv("DB_PASSWORD", "")
